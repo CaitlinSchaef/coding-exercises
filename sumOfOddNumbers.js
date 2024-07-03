afe48e4dd95f11690f5c35e1ca556090cf22025d
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-07-03 14:28:13 -0400

    added one weird one to do later

---
+++ sumOfOddNumbers.js
@@ -0,0 +1,22 @@
+/*
+https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+
+Instructions: Given the triangle of consecutive odd numbers, Calculate the sum of the numbers in the nth row of this triangle 
+(starting at index 1) e.g.: (Input --> Output)
+Notes: It is a triangle of odd numbers is all 
+Example:  
+1 -->  1
+2 --> 3 + 5 = 8
+Params: given integers
+Return: return integer (sum)
+Concerns: identifying the correct row (seems like the chess board exercise from awhile ago)
+Solutions: 
+Given Code: 
+function rowSumOddNumbers(n) {
+	// TODO
+}
+*/
+
+function rowSumOddNumbers(n) {
+    
+}
